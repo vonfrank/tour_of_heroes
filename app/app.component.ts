ae9d5eacb83490db7c64cba8c40a67a374eb9338
Henrik Bolding Frank <henrik@vonfrank.dk>
2016-10-25 07:30:23 +0200

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root-component',
+  template: '<h1>Tour of Heroes</h1>'
+})
+
+export class AppComponent { 
+    title = 'Tour of Heroes';
+    hero = 'windstorm';
+}
